3fec8029a0eb40a426a6dc366638d3c6984cf4bd
daberdiel <dberdiel@chicagotechacademy.org>
2015-09-16 12:06:00 -0500

    Create assignment4.rb

---
+++ assignment4.rb
@@ -0,0 +1,6 @@
+puts "How many hours are you at school?"
+hours = gets.to_i  "7"
+puts "How many classes do you have?"
+classes = gets.to_i  "7"
+puts "How many days do you have school in one week?"
+days = gets.to_i  "5" 
