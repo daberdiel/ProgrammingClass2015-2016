75eb13111183870889388a5f928ebb08cdf4061b
daberdiel <dberdiel@chicagotechacademy.org>
2015-09-28 11:20:28 -0500

    Create assignment1.rb

---
+++ assignment1.rb
@@ -0,0 +1,12 @@
+PeanutButterJellyRecipe
+
+Gather all materials Materials include: Plate, Bread (2 slices), jar of peanut butter, jar of jelly, and a spoon
+
+Place your plate on a table and then place the bread on the plate
+Get your Peanut Butter and twist the top off. Do the same for the jar of Jelly
+Get your spoon and scoop out some Peanut Butter
+Spread your Peanut Butter across one slice of bread
+Now scoop out some jelly
+Spread your jelly across your second piece of bread
+Take both of your slices of bread with the Peanut butter and jelly and put them together with the pb&j facing each other
+Enjoy
