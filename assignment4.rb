5e83d6f388349c2692d939d31d9f02868f26ecfc
daberdiel <dberdiel@chicagotechacademy.org>
2015-10-05 10:57:17 -0500

    Update assignment4.rb

--- assignment4.rb
+++ assignment4.rb
@@ -1,6 +1,6 @@
 puts "How many hours are you at school?"
-hours = gets.to_i  "7"
+hours = gets.to_i
 puts "How many classes do you have?"
-classes = gets.to_i  "7"
+classes = gets.to_i
 puts "How many days do you have school in one week?"
-days = gets.to_i  "5" 
+days = gets.to_i
